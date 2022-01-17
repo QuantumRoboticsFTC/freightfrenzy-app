09e203ab8f75bdfeac084f633239081b02572136
Mircea Roata <mircearoatapalade@gmail.com>
2022-01-17 16:54:50 +0200

    Automated Carousel

--- TeamCode/src/main/java/eu/qrobotics/freightfrenzy/teamcode/subsystems/Carousel.java
+++ TeamCode/src/main/java/eu/qrobotics/freightfrenzy/teamcode/subsystems/Carousel.java
@@ -4,25 +4,41 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
 public class Carousel implements Subsystem {
 
-    public double carouselPower;
-
     private DcMotor carouselMotor;
+
+    public static double ACCELERATION_RATE = 0.7; // power increase / second
+    public static double TIME = 2.0; // seconds
 
     Carousel(HardwareMap hardwareMap, Robot robot) {
         carouselMotor = hardwareMap.get(DcMotor.class, "carouselMotor");
 
-        carouselPower = 0;
+        carouselMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public static boolean IS_DISABLED = false;
 
+    ElapsedTime timer = new ElapsedTime(0);
+
+    public void spin() {
+        timer.reset();
+    }
+
+    public void stopSpin() {
+        timer = new ElapsedTime(0);
+    }
+
     @Override
     public void update() {
         if(IS_DISABLED) return;
+        double carouselPower = 0;
+        if(timer.seconds() < TIME) {
+            carouselPower = ACCELERATION_RATE * timer.seconds();
+        }
         carouselMotor.setPower(carouselPower);
     }
 }
